2ae7738cbfd99d794fc38e30940d7df2d277f104
MateusHoffman <mateushoffmandev@gmail.com>
2022-11-07 21:06:23 -0300

    8 done

--- src/services/vilidations/inputValidatons.ts
+++ src/services/vilidations/inputValidatons.ts
@@ -4,7 +4,7 @@ import { postProductSchema, postUserSchema, postOrdersSchema } from './schema';
 const validatePostProduct = async (req: Request, res: Response, next: NextFunction) => {
   const objInputPost = req.body;
   const { error } = postProductSchema.validate(objInputPost);
-  const err: any = error;
+  const err = error;
   if (!err) return next();
   if (err.message.includes('required')) return res.status(400).json({ message: err.message });
   if (err.message.includes('must')) return res.status(422).json({ message: err.message });
@@ -14,7 +14,7 @@ const validatePostProduct = async (req: Request, res: Response, next: NextFuncti
 const validatePostUser = async (req: Request, res: Response, next: NextFunction) => {
   const objInputPost = req.body;
   const { error } = postUserSchema.validate(objInputPost);
-  const err: any = error;
+  const err = error;
   if (!err) return next();
   if (err.message.includes('required')) return res.status(400).json({ message: err.message });
   if (err.message.includes('must')) return res.status(422).json({ message: err.message });
@@ -24,7 +24,7 @@ const validatePostUser = async (req: Request, res: Response, next: NextFunction)
 const validatePostOrders = async (req: Request, res: Response, next: NextFunction) => {
   const objInputPost = req.body;
   const { error } = postOrdersSchema.validate(objInputPost);
-  const err: any = error;
+  const err = error;
   if (!err) return next();
   if (err.message.includes('required')) return res.status(400).json({ message: err.message });
   if (err.message.includes('must')) return res.status(422).json({ message: err.message });
